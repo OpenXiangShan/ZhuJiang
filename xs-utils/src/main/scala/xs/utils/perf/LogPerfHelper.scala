e45e88eb32f15f5ad955780cb9262146d230fd6d
Ruomio <1065940593@qq.com>
2026-07-23 14:35:57 +0800

    fix(zhujiang): fix emu compiling by gsim

--- xs-utils/src/main/scala/xs/utils/perf/LogPerfHelper.scala
+++ xs-utils/src/main/scala/xs/utils/perf/LogPerfHelper.scala
@@ -13,6 +13,24 @@ class LogPerfIO extends Bundle {
 class LogPerfHelper extends BlackBox with HasBlackBoxInline {
   val io = IO(Output(new LogPerfIO))
 
+  private val cppExtModule =
+    """
+      |void LogPerfHelper(
+      |  uint64_t& timer,
+      |  uint8_t&  logEnable,
+      |  uint8_t&  clean,
+      |  uint8_t&  dump
+      |) {
+      |  // gsim compiles extensions outside the generated SimTop methods, so the
+      |  // SystemVerilog-only top-level control signals are not in scope here.
+      |  timer     = 0;
+      |  logEnable = 0;
+      |  clean     = 0;
+      |  dump      = 0;
+      |}
+      |""".stripMargin
+  difftest.DifftestModule.createCppExtModule("LogPerfHelper", cppExtModule)
+
   val sverilog =
     """`ifndef SIM_TOP_MODULE_NAME
       |  `define SIM_TOP_MODULE_NAME SimTop
