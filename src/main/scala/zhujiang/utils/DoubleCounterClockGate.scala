f1ada56a2d0bf5259ba9cfa158a4300a01f49a39
zhengzhongqiang <3486829357@qq.com>
2026-09-09 15:26:00 +0800

    fix(perf): wake dynamic clocks for counter dumps
    Force dynamically gated ZhuJiang domains awake for
    performance simulation clean and dump pulses. This lets idle HN and AXI
    counter blocks observe the global performance control signals.
    
    Elide the wake path when performance printing is disabled.

--- src/main/scala/zhujiang/utils/DoubleCounterClockGate.scala
+++ src/main/scala/zhujiang/utils/DoubleCounterClockGate.scala
@@ -2,8 +2,11 @@ package zhujiang.utils
 
 import chisel3._
 import chisel3.util._
+import org.chipsalliance.cde.config.Parameters
+import xs.utils.perf.LogPerfHelper
+import zhujiang.perf.ZJPerf
 
-class DoubleCounterClockGate(ckenWindow: Int = 7, idleWindow: Int = 7, testctl: Boolean = false) extends Module {
+class DoubleCounterClockGate(ckenWindow: Int = 7, idleWindow: Int = 7, testctl: Boolean = false)(implicit p: Parameters) extends Module {
     override val desiredName = "DoubleCounterICG"
     val io = IO(new Bundle {
         val te       = Input(Bool())
@@ -19,7 +22,13 @@ class DoubleCounterClockGate(ckenWindow: Int = 7, idleWindow: Int = 7, testctl: 
     private val cken          = RegInit(true.B)
     private val active        = RegInit(false.B)
     private val cg            = Module(new xs.utils.ClockGate)
-    cg.io.TE  := io.te | io.test_on.getOrElse(false.B)
+    private val perfWake = if (ZJPerf.enabled) {
+        val ctrl = Module(new LogPerfHelper).io
+        ctrl.clean | ctrl.dump
+    } else {
+        false.B
+    }
+    cg.io.TE  := io.te | io.test_on.getOrElse(false.B) | perfWake
     cg.io.CK  := clock
     io.ock    := cg.io.Q
     cg.io.E   := Mux(io.test_off.getOrElse(false.B), false.B, cken | io.inbound)
